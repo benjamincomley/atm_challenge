6b2d61e573b587bb3a882f07ca69e469219a0e1a
benji <benjamincomley@gmail.com>
2017-04-04 09:41:37 +0200

    added insufficient funds code

--- lib/atm.rb
+++ lib/atm.rb
@@ -11,11 +11,11 @@ class Atm
   def withdraw(amount, account)
     case
     when insufficient_funds_in_account?(amount, account)
-        return
-      else
-        perform_transaction(amount, account)
-      end
+      { status: false, message: 'insufficient funds', date: Date.today }
+    else
+      perform_transaction(amount, account)
     end
+  end
 
   private
 
